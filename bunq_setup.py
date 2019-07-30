5258e9555b693d70d5f8ea67f26f3bb6d3a47cd2
Noah Van Es <noahves@vub.ac.be>
2019-07-30 19:08:02 +0200

    fixed bunq_setup.py

--- bunq_setup.py
+++ bunq_setup.py
@@ -4,7 +4,7 @@ import bunq.sdk.context as bunqContext
 
 BUNQ_CONF = 'bunq.conf'
 
-if __name__ == 'main':
+if __name__ == '__main__':
     parser = OptionParser()
     parser.add_argument('--api-key',dest='api_key',help='the bunq API key to configure the server')
     args = parser.parse_args()
