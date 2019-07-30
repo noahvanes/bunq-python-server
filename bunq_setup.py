cdb6346ef6de8ca5f708d06d1444319fa3aeb5af
Noah Van Es <noahves@vub.ac.be>
2019-07-30 19:10:11 +0200

    bunq_setup fix

--- bunq_setup.py
+++ bunq_setup.py
@@ -4,14 +4,14 @@ import bunq.sdk.context as bunqContext
 
 BUNQ_CONF = 'bunq.conf'
 
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--api-key',dest='api_key',help='the bunq API key to configure the server')
-    args = parser.parse_args()
-    bunq_setup_configuration(api_key)
-
 def bunq_setup_configuration(api_key):
     env_type = bunqContext.ApiEnvironmentType.PRODUCTION
     description = socket.gethostname()
     api_context = bunqContext.ApiContext(env_type, api_key, description)
     api_context.save(BUNQ_CONF)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--api-key',dest='api_key',help='the bunq API key to configure the server')
+    args = parser.parse_args()
+    bunq_setup_configuration(api_key)
